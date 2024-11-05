614927fb445dc7fb6bf04c464518178b779cfcca
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2024-11-05 08:56:07 -0700

    add stripe hook logic

--- internal/ent/hooks/event.go
+++ internal/ent/hooks/event.go
@@ -2,11 +2,14 @@ package hooks
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"entgo.io/ent"
 
 	"github.com/rs/zerolog/log"
+	"github.com/stripe/stripe-go/v81"
+	scust "github.com/stripe/stripe-go/v81/customer"
 
 	entgen "github.com/theopenlane/core/internal/ent/generated"
 	"github.com/theopenlane/core/pkg/events/soiree"
@@ -22,6 +25,10 @@ func NewEntEvent(topic string, payload interface{}) *EntEvent {
 	}
 }
 
+type Output struct {
+	ID string `json:"id"`
+}
+
 func EmitEventHook(pool *soiree.EventPool) ent.Hook {
 	return func(next ent.Mutator) ent.Mutator {
 		return ent.MutateFunc(func(ctx context.Context, mutation ent.Mutation) (ent.Value, error) {
@@ -35,7 +42,18 @@ func EmitEventHook(pool *soiree.EventPool) ent.Hook {
 
 			fields := mutation.Fields()
 
+			out, err := json.Marshal(retVal)
+			if err != nil {
+				log.Err(err).Msg("Failed to marshal return value")
+			}
+
+			other := Output{}
+			json.Unmarshal(out, &other)
+
 			event := NewEntEvent(fmt.Sprintf("%s.%s", typ, op), mutation)
+
+			event.Properties().Set("ID", other.ID)
+
 			for _, field := range fields {
 				value, exists := mutation.Field(field)
 				if exists {
@@ -92,23 +110,72 @@ func RegisterListeners(pool *soiree.EventPool) {
 
 func handleCustomerCreate(event soiree.Event) error {
 	log.Info().Msg("Handling organization create event")
+
+	stripe.Key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+
 	props := event.Properties()
-	for k, v := range props {
-		log.Info().Msgf("Key: %s, Value: %v", k, v)
+	// this map is empty?
+	log.Info().Msgf("Event properties from listener: %v", props)
+
+	billingEmail, exists := props["description"]
+	if exists && billingEmail != "" {
+		log.Info().Msgf("Billing email: %s", billingEmail)
+		email := billingEmail.(string)
+		customerParams := &stripe.CustomerListParams{
+			Email: &email,
+		}
+
+		i := scust.List(customerParams)
+
+		if !i.Next() {
+			// Customer does not exist, create a new one
+			customerParams := &stripe.CustomerParams{
+				Email: &email,
+			}
+
+			customer, err := scust.New(customerParams)
+			if err != nil {
+				log.Err(err).Msg("Failed to create Stripe customer")
+				return err
+			}
+			// Store the Stripe customer ID in the OrganizationSetting record
+			orgID := props["ID"].(string)
+			// Assuming you have a function to update the OrganizationSetting record
+			err = updateOrganizationSettingWithCustomerID(orgID, customer.ID)
+			if err != nil {
+				log.Err(err).Msg("Failed to update OrganizationSetting with Stripe customer ID")
+				return err
+			}
+		}
 	}
 
-	// log.Info().Msgf("Organization Name: %s", name)
-	// log.Info().Msgf("Event Topic: %s", event.Topic())
-	//	mutation.Client().OrganizationSetting.Query().Where(organizationsetting.OrganizationID(id)).Only(context.Background())
+	return nil
+}
 
-	//	customerParams := &stripe.CustomerParams{
-	//        Email: mutation.setting.
-	//		        Name:  &name,
-	//	}
-	//	_, err := scust.New(customerParams)
-	//	if err != nil {
-	//		log.Printf("Failed to create Stripe customer: %v", err)
-	//	}
+// Add this function to update the OrganizationSetting record with the Stripe customer ID
+func updateOrganizationSettingWithCustomerID(orgID, customerID string) error {
+	client := entgen.FromContext(context.Background())
+	err := client.Organization.UpdateOneID(orgID).SetTags([]string{customerID}).Exec(context.Background())
+	if err != nil {
+		log.Err(err).Msgf("Failed to update OrganizationSetting ID %s with Stripe customer ID %s", orgID, customerID)
+		return err
+	}
+
+	log.Info().Msgf("Updated OrganizationSetting ID %s with Stripe customer ID %s", orgID, customerID)
+
+	return nil
+}
+
+func CreateStripeCustomerIfNotExists(name, email string) error {
+	customerParams := &stripe.CustomerParams{
+		Email: &email,
+		Name:  &name,
+	}
+
+	_, err := scust.New(customerParams)
+	if err != nil {
+		log.Printf("Failed to create Stripe customer: %v", err)
+	}
 
 	return nil
 }
